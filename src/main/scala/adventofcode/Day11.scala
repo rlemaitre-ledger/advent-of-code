4cdd425fb80bda6116ebe1a1e43978b548288ab5
Raphaël Lemaitre <raphael.lemaitre@ledger.fr>
2022-12-11 18:17:21 +0100

    ✨ (Day 11): Complete part 2

--- src/main/scala/adventofcode/Day11.scala
+++ src/main/scala/adventofcode/Day11.scala
@@ -2,18 +2,21 @@ package adventofcode
 
 import adventofcode.Day11.monkeys
 
-object Day11 extends AdventOfCodeBase[Int, Int]("day11.txt") {
-  override def part1(lines: List[String]): Int = business(play(monkeys(lines), 20))
-  override def part2(lines: List[String]): Int = ???
-  def monkeys(lines: List[String]): List[Monkey] =
-    lines.grouped(7).map(Monkey.parse).toList.sortBy(_.number)
-  def business(monkeys: List[Monkey]): Int = monkeys.map(_.inspections).sorted.reverse.take(2).product
+object Day11 extends AdventOfCodeBase[Long, Long]("day11.txt") {
+  override def part1(lines: List[String]): Long = business(play(monkeys(lines, Mode.Part1), 20))
+  override def part2(lines: List[String]): Long = business(play(monkeys(lines, Mode.Part2), 10000))
+  def monkeys(lines: List[String], mode: Mode): List[Monkey] = {
+    val monkeys  = lines.grouped(7).map(l => Monkey.parse(l, mode)).toList.sortBy(_.number)
+    val dividers = monkeys.map(_.test.divisibleBy).toSet
+    monkeys.map(m => m.copy(items = m.items.map(item => dividers.foldLeft(item)((i, mod) => i.addModulo(mod)))))
+  }
+  def business(monkeys: List[Monkey]): Long = monkeys.map(_.inspections).sorted.reverse.take(2).product
   def play(monkeys: List[Monkey], nbRounds: Int): List[Monkey] =
     (1 to nbRounds).foldLeft(monkeys)((m, _) => round(m))
   def round(monkeys: List[Monkey]): List[Monkey] = {
     def roundFor(state: List[Monkey], n: Int): List[Monkey] = {
       val current = state(n)
-      val result  = current.inspectItems
+      val result  = current.inspectItems()
       result.moves.foldLeft(state.updated(n, result.updated)) { case (list, (index, toMove)) =>
         list.updated(index, list(index).receive(toMove))
       }
@@ -21,8 +24,8 @@ object Day11 extends AdventOfCodeBase[Int, Int]("day11.txt") {
     monkeys.indices.foldLeft(monkeys)(roundFor)
   }
 
-  final case class Monkey(number: Int, items: List[Item], operation: Operation, test: Test, inspections: Int) {
-    def inspectItems: InspectionResult =
+  final case class Monkey(number: Int, items: List[Item], operation: Operation, test: Test, inspections: Long) {
+    def inspectItems(): InspectionResult =
       InspectionResult(
         copy(items = List.empty, inspections = inspections + items.size),
         items.map(operation.modify).map(_.afterInspection).foldLeft(Map.empty[Int, List[Item]]) { (moves, item) =>
@@ -37,10 +40,10 @@ object Day11 extends AdventOfCodeBase[Int, Int]("day11.txt") {
   }
   final case class InspectionResult(updated: Monkey, moves: Map[Int, List[Item]])
   object Monkey {
-    def parse(input: List[String]): Monkey =
+    def parse(input: List[String], mode: Mode): Monkey =
       Monkey(
         number = parseNumber(input.head),
-        items = Item.parse(input(1)),
+        items = Item.parse(input(1), mode),
         operation = Operation.parse(input(2)),
         test = Test.parse(input(3), input(4), input(5)),
         inspections = 0
@@ -48,25 +51,57 @@ object Day11 extends AdventOfCodeBase[Int, Int]("day11.txt") {
     def parseNumber(str: String): Int = str match
       case s"Monkey ${nb}:" => nb.toInt
   }
-  final case class Item(worryLevel: Int) {
-    def afterInspection: Item = Item(worryLevel / 3)
+  sealed trait Item {
+    def add(increment: Int): Item
+    def multiply(factor: Int): Item
+    def square: Item
+    def isMultipleOf(nb: Int): Boolean
+    def addModulo(mod: Int): Item
+    def afterInspection: Item
+  }
+  final case class SimpleItem(worryLevel: Int) extends Item {
+    override def add(increment: Int): Item      = SimpleItem(worryLevel + increment)
+    override def multiply(factor: Int): Item    = SimpleItem(worryLevel * factor)
+    override def square: Item                   = SimpleItem(worryLevel * worryLevel)
+    override def isMultipleOf(nb: Int): Boolean = worryLevel % nb == 0
+    override def afterInspection: Item          = SimpleItem(worryLevel / 3)
+    override def addModulo(mod: Int): Item      = this
+  }
+  final case class ModuloItem(initialValue: Int, dividers: Map[Int, Int]) extends Item {
+    override def add(increment: Int): Item      = copy(dividers = dividers.map((k, v) => (k, (v + increment) % k)))
+    override def multiply(factor: Int): Item    = copy(dividers = dividers.map((k, v) => (k, (v * factor) % k)))
+    override def square: Item                   = copy(dividers = dividers.map((k, v) => (k, (v * v) % k)))
+    override def isMultipleOf(nb: Int): Boolean = dividers.get(nb).contains(0)
+    override def afterInspection: Item          = this
+    override def addModulo(mod: Int): Item      = copy(dividers = dividers.updated(mod, initialValue % mod))
+  }
+  object ModuloItem {
+    def apply(n: Int): ModuloItem = ModuloItem(n, Map.empty)
   }
   object Item {
-    def parse(str: String): List[Item] = str match
-      case s"  Starting items: ${levels}" => levels.split(',').map(_.trim.toInt).map(Item.apply).toList
-
+    def parse(str: String, mode: Mode): List[Item] = str match
+      case s"  Starting items: ${levels}" =>
+        levels
+          .split(',')
+          .map(s => s.trim.toInt)
+          .map(n =>
+            mode match
+              case Mode.Part1 => SimpleItem(n)
+              case Mode.Part2 => ModuloItem(n)
+          )
+          .toList
   }
   sealed trait Operation {
     def modify(item: Item): Item
   }
   case class Increase(increment: Int) extends Operation {
-    override def modify(item: Item): Item = Item(item.worryLevel + increment)
+    override def modify(item: Item): Item = item.add(increment)
   }
   case class Multiply(factor: Int) extends Operation {
-    override def modify(item: Item): Item = Item(item.worryLevel * factor)
+    override def modify(item: Item): Item = item.multiply(factor)
   }
   case object Square extends Operation {
-    override def modify(item: Item): Item = Item(item.worryLevel * item.worryLevel)
+    override def modify(item: Item): Item = item.square
   }
   object Operation {
     def parse(str: String): Operation = str match
@@ -75,7 +110,7 @@ object Day11 extends AdventOfCodeBase[Int, Int]("day11.txt") {
       case s"  Operation: new = old + ${increment}" => Increase(increment.toInt)
   }
   case class Test(divisibleBy: Int, ifTrue: Int, ifFalse: Int) {
-    def test(item: Item): Int = if (item.worryLevel % divisibleBy == 0) ifTrue else ifFalse
+    def test(item: Item): Int = if (item.isMultipleOf(divisibleBy)) ifTrue else ifFalse
   }
   object Test {
     def parse(testLine: String, ifTrueLine: String, ifFalseLine: String): Test =
@@ -84,5 +119,8 @@ object Day11 extends AdventOfCodeBase[Int, Int]("day11.txt") {
       case s"  Test: divisible by ${nb}" => nb.toInt
     def parseAction(str: String): Int = str match
       case s"    If ${_}: throw to monkey ${monkey}" => monkey.toInt
+  }
+  enum Mode {
+    case Part1, Part2
   }
 }
--- src/test/scala/adventofcode/Day11Test.scala
+++ src/test/scala/adventofcode/Day11Test.scala
@@ -30,33 +30,129 @@ class Day11Test extends AoCTest {
                                  |  Test: divisible by 17
                                  |    If true: throw to monkey 0
                                  |    If false: throw to monkey 1""".stripMargin
-  val monkeys: List[Monkey] = List(
+  val round0: List[Monkey] = List(
     Monkey(
       number = 0,
-      items = List(Item(79), Item(98)),
+      items = List(SimpleItem(79), SimpleItem(98)),
       operation = Multiply(19),
       test = Test(23, 2, 3),
       inspections = 0
     ),
     Monkey(
       number = 1,
-      items = List(Item(54), Item(65), Item(75), Item(74)),
+      items = List(SimpleItem(54), SimpleItem(65), SimpleItem(75), SimpleItem(74)),
       operation = Increase(6),
       test = Test(19, 2, 0),
       inspections = 0
     ),
     Monkey(
       number = 2,
-      items = List(Item(79), Item(60), Item(97)),
+      items = List(SimpleItem(79), SimpleItem(60), SimpleItem(97)),
       operation = Square,
       test = Test(13, 1, 3),
       inspections = 0
     ),
-    Monkey(number = 3, items = List(Item(74)), operation = Increase(3), test = Test(17, 0, 1), inspections = 0)
+    Monkey(number = 3, items = List(SimpleItem(74)), operation = Increase(3), test = Test(17, 0, 1), inspections = 0)
+  )
+  val round1: List[Monkey] = List(
+    Monkey(
+      number = 0,
+      items = List(60, 71, 81, 80).map(SimpleItem.apply),
+      operation = Multiply(19),
+      test = Test(23, 2, 3),
+      inspections = 2
+    ),
+    Monkey(
+      number = 1,
+      items = List(77, 1504, 1865, 6244, 3603, 9412).map(SimpleItem.apply),
+      operation = Increase(6),
+      test = Test(19, 2, 0),
+      inspections = 4
+    ),
+    Monkey(
+      number = 2,
+      items = List.empty,
+      operation = Square,
+      test = Test(13, 1, 3),
+      inspections = 3
+    ),
+    Monkey(number = 3, items = List.empty, operation = Increase(3), test = Test(17, 0, 1), inspections = 6)
+  )
+  val round2: List[Monkey] = List(
+    Monkey(
+      number = 0,
+      items = List(83, 1510, 1871, 6250, 3609, 9418).map(SimpleItem.apply),
+      operation = Multiply(19),
+      test = Test(23, 2, 3),
+      inspections = 6
+    ),
+    Monkey(
+      number = 1,
+      items = List(1143, 1352, 1542, 1523).map(SimpleItem.apply),
+      operation = Increase(6),
+      test = Test(19, 2, 0),
+      inspections = 10
+    ),
+    Monkey(
+      number = 2,
+      items = List.empty,
+      operation = Square,
+      test = Test(13, 1, 3),
+      inspections = 3
+    ),
+    Monkey(number = 3, items = List.empty, operation = Increase(3), test = Test(17, 0, 1), inspections = 10)
+  )
+  val round3: List[Monkey] = List(
+    Monkey(
+      number = 0,
+      items = List(1149, 1358, 1548, 1529).map(SimpleItem.apply),
+      operation = Multiply(19),
+      test = Test(23, 2, 3),
+      inspections = 12
+    ),
+    Monkey(
+      number = 1,
+      items = List(1580, 28693, 35552, 118753, 68574, 178945).map(SimpleItem.apply),
+      operation = Increase(6),
+      test = Test(19, 2, 0),
+      inspections = 14
+    ),
+    Monkey(
+      number = 2,
+      items = List.empty,
+      operation = Square,
+      test = Test(13, 1, 3),
+      inspections = 3
+    ),
+    Monkey(number = 3, items = List.empty, operation = Increase(3), test = Test(17, 0, 1), inspections = 16)
+  )
+  val round4: List[Monkey] = List(
+    Monkey(
+      number = 0,
+      items = List(1586, 28699, 35558, 118759, 68580, 178951).map(SimpleItem.apply),
+      operation = Multiply(19),
+      test = Test(23, 2, 3),
+      inspections = 16
+    ),
+    Monkey(
+      number = 1,
+      items = List(21834, 25805, 29415, 29054).map(SimpleItem.apply),
+      operation = Increase(6),
+      test = Test(19, 2, 0),
+      inspections = 20
+    ),
+    Monkey(
+      number = 2,
+      items = List.empty,
+      operation = Square,
+      test = Test(13, 1, 3),
+      inspections = 3
+    ),
+    Monkey(number = 3, items = List.empty, operation = Increase(3), test = Test(17, 0, 1), inspections = 20)
   )
 
   test("part 1") {
-    assertEquals(Day11.part1(input), 10605)
+    assertEquals(Day11.part1(input), 10605L)
   }
   test("part 2") {
     intercept[Throwable] {
@@ -64,41 +160,36 @@ class Day11Test extends AoCTest {
     }
   }
   test("parse monkeys") {
-    assertEquals(Day11.monkeys(input), monkeys)
+    assertEquals(Day11.monkeys(input, Mode.Part1), round0)
   }
   test("test item") {
-    assertEquals(Test(2, 12, 42).test(Item(10)), 12)
-    assertEquals(Test(2, 12, 42).test(Item(11)), 42)
+    assertEquals(Test(2, 12, 42).test(SimpleItem(10)), 12)
+    assertEquals(Test(2, 12, 42).test(SimpleItem(11)), 42)
   }
   test("increase operation") {
-    assertEquals(Increase(1).modify(Item(10)), Item(11))
+    assertEquals(Increase(1).modify(SimpleItem(10)), SimpleItem(11))
   }
   test("multiply operation") {
-    assertEquals(Multiply(3).modify(Item(10)), Item(30))
+    assertEquals(Multiply(3).modify(SimpleItem(10)), SimpleItem(30))
   }
   test("square operation") {
-    assertEquals(Square.modify(Item(10)), Item(100))
-  }
-  test("item after inspection") {
-    assertEquals(Item(1500).afterInspection, Item(500))
-    assertEquals(Item(1501).afterInspection, Item(500))
-    assertEquals(Item(1502).afterInspection, Item(500))
-    assertEquals(Item(1503).afterInspection, Item(501))
+    assertEquals(Square.modify(SimpleItem(10)), SimpleItem(100))
   }
   test("play round 1") {
     assertEquals(
-      Day11.round(monkeys),
+      Day11.round(round0),
       List(
         Monkey(
           number = 0,
-          items = List(Item(20), Item(23), Item(27), Item(26)),
+          items = List(SimpleItem(20), SimpleItem(23), SimpleItem(27), SimpleItem(26)),
           operation = Multiply(19),
           test = Test(23, 2, 3),
           inspections = 2
         ),
         Monkey(
           number = 1,
-          items = List(Item(2080), Item(25), Item(167), Item(207), Item(401), Item(1046)),
+          items =
+            List(SimpleItem(2080), SimpleItem(25), SimpleItem(167), SimpleItem(207), SimpleItem(401), SimpleItem(1046)),
           operation = Increase(6),
           test = Test(19, 2, 0),
           inspections = 4
@@ -114,4 +205,16 @@ class Day11Test extends AoCTest {
       )
     )
   }
+  test("inspections with new rules") {
+    testInspections(1, List(2L, 4L, 3L, 6L))
+    testInspections(20, List(99L, 97L, 8L, 103L))
+    testInspections(1000, List(5204L, 4792L, 199L, 5192L))
+    testInspections(2000, List(10419L, 9577L, 392L, 10391L))
+    testInspections(10000, List(52166L, 47830L, 1938L, 52013L))
+  }
+  def testInspections(nbRounds: Int, expected: List[Long]): Unit =
+    assertEquals(
+      play(Day11.monkeys(input, Mode.Part2), nbRounds).map(_.inspections),
+      expected
+    )
 }
