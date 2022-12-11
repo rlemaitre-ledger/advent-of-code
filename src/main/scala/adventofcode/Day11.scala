e92e427456a881bbdaa91945dea666996dcb7067
Raphaël Lemaitre <raphael.lemaitre@ledger.fr>
2022-12-11 11:50:20 +0100

    ✨ (Day 11): Initialize day 11

---
+++ src/main/scala/adventofcode/Day11.scala
@@ -0,0 +1,7 @@
+package adventofcode
+
+object Day11 extends AdventOfCodeBase[Int, Int]("day11.txt"){
+  override def part1(lines: List[String]): Int = ???
+
+  override def part2(lines: List[String]): Int = ???
+}
