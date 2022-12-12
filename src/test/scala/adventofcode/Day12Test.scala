d7b7602e75929299a9063540ecab08ab63e66009
Raphaël Lemaitre <raphael.lemaitre@ledger.fr>
2022-12-12 12:32:14 +0100

    ✨ (Day 12): Initialize Day 12

---
+++ src/test/scala/adventofcode/Day12Test.scala
@@ -0,0 +1,21 @@
+package adventofcode
+
+import adventofcode.Day12.*
+
+class Day12Test extends AoCTest {
+  override val lines: String = """Sabqponm
+                                 |abcryxxl
+                                 |accszExk
+                                 |acctuvwj
+                                 |abdefghi""".stripMargin
+  test("part 1") {
+    intercept[NotImplementedError] {
+      assertEquals(part1(input), 31)
+    }
+  }
+  test("part 2") {
+    intercept[NotImplementedError] {
+      assertEquals(part2(input), 31)
+    }
+  }
+}
