0f1e3f6486abc1eb045a0b77300757f3de6d2da5
Raphaël Lemaitre <raphael.lemaitre@ledger.fr>
2022-12-22 11:02:15 +0100

    ✨ (2021-04): Complete Day 4 of 2021

---
+++ src/main/scala/adventofcode/aoc2021/day04/Board.scala
@@ -0,0 +1,42 @@
+package adventofcode.aoc2021.day04
+
+import adventofcode.aoc2021.day04.Board.*
+import adventofcode.utils.coordinates.Coordinates
+
+final case class Board(numbers: Map[Int, Cell], lastDraw: Int = -1, drawCount: Int = 0) {
+  private def columns: Seq[Iterable[Cell]] = (0 until 5).map { i =>
+    numbers.filter((_, cell) => cell.coordinates.x == i).values.toList.sortBy(_.coordinates.y)
+  }.toList
+  private def rows: Seq[Iterable[Cell]] = (0 until 5).map { i =>
+    numbers.filter((_, cell) => cell.coordinates.y == i).values.toList.sortBy(_.coordinates.x)
+  }.toList
+  def wins: Boolean = columns.exists(_.forall(_.drawn)) || rows.exists(_.forall(_.drawn))
+  def score: Int    = numbers.values.filterNot(_.drawn).map(_.value).sum * lastDraw
+  def draw(number: Int): Board =
+    if numbers.contains(number) then
+      copy(numbers = numbers + (number -> numbers(number).mark), lastDraw = number, drawCount = drawCount + 1)
+    else copy(lastDraw = number, drawCount = drawCount + 1)
+}
+object Board {
+  final case class Cell(value: Int, coordinates: Coordinates, drawn: Boolean = false) {
+    def mark: Cell = copy(drawn = true)
+  }
+  def parseLines(lines: List[String]): List[Board] =
+    def parseLine(line: String, index: Int): List[Cell] =
+      println(s"parse line $index ($line)")
+      line.split("\\W+").dropWhile(_.isEmpty).zipWithIndex.map((n, i) => Cell(n.toInt, Coordinates(i, index))).toList
+    lines
+      .grouped(5)
+      .map { grid =>
+        Board(
+          (
+            parseLine(grid(0), 0) ++
+              parseLine(grid(1), 1) ++
+              parseLine(grid(2), 2) ++
+              parseLine(grid(3), 3) ++
+              parseLine(grid(4), 4)
+          ).map(cell => cell.value -> cell).toMap
+        )
+      }
+      .toList
+}
---
+++ src/main/scala/adventofcode/aoc2021/day04/Day04.scala
@@ -0,0 +1,27 @@
+package adventofcode.aoc2021.day04
+import adventofcode.Problem
+import adventofcode.inputLines
+import scala.annotation.tailrec
+
+final case class Day04(input: List[String]) extends Problem[List[String], Int, Int](2021, 4, "Giant Squid") {
+  private val draws: List[Int]    = input.head.split(',').map(_.toInt).toList
+  private val boards: List[Board] = Board.parseLines(input.tail)
+  override def part1: Int         = findFirstScore(draws, boards)
+
+  override def part2: Int = findLastScore(draws, boards)
+  @tailrec
+  private def findFirstScore(remainingDraws: List[Int], playedBoards: List[Board]): Int =
+    playedBoards.find(_.wins) match
+      case Some(value: Board) => value.score
+      case None               => findFirstScore(remainingDraws.tail, playedBoards.map(_.draw(remainingDraws.head)))
+
+  @tailrec
+  private def findLastScore(remainingDraws: List[Int], playedBoards: List[Board]): Int = {
+    val notWinning = playedBoards.filterNot(_.wins)
+    if notWinning.length == 1 then findFirstScore(remainingDraws, notWinning)
+    else findLastScore(remainingDraws.tail, playedBoards.map(_.draw(remainingDraws.head)))
+  }
+}
+object Day04 {
+  val instance: Day04 = Day04(inputLines("2021/day04.txt").filterNot(_.isEmpty))
+}
